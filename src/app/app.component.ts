9523a4035b8da151b19f79708fddb2cb1f6c118d
raulsf10 <ivanraul10@gmail.com>
2022-09-15 13:28:24 -0600

    Esta fue mi solución

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-calculadora';
+  operandoA:number = 0;
+  operandoB:number = 0;
+  resultado:number = 0;
+
+  sumar(){
+    this.resultado = (this.operandoA + this.operandoB);
+  }
 }
